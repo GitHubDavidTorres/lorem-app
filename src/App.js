94037881c757b667825fc676c58840756e837e8b
GitHubDavidTorres <david.torres.santander@gmail.com>
2021-12-18 20:18:55 -0300

    master

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     if (count <= 0) {
       amount = 1;
     }
-    if (count > 8) {
-      amount = 8;
+    if (count > 9) {
+      amount = 9;
     }
 
     setText(data.slice(0, amount));
